f70e2f5b228a1e675162bfff3394c06fb6c87d72
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-08 13:21:18 +0600

    read book list done

---
+++ src/Components/ListingBook/ReadBook.jsx
@@ -0,0 +1,69 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../../API Context/APIContext';
+import { IoLocationOutline } from "react-icons/io5";
+import { GrDocumentUser } from "react-icons/gr";
+import { MdManageAccounts } from "react-icons/md";
+
+const ReadBook = () => {
+    const { readBook } = useContext(BookContext);
+    return (
+        <div>
+            {
+                readBook.map(book => <div key={book.bookId}>
+                    <div className='flex gap-9 mt-10'>
+                        <div className='bg-[#13131315] py-14 px-20'>
+                            <img className='h-30' src={book.image} alt={book.bookName} />
+                        </div>
+                        <div>
+                            <h1
+                                className='playfair font-bold text-2xl text-[#131313] mb-4'>{book.bookName}
+                            </h1>
+                            <p className='text-[#13131380] font-medium mb-4'>{book.author}</p>
+                            <div className='mb-4 flex items-center gap-4'>
+                                <p className='font-bold text-[#131313]'>Tag</p>
+                                <span className='flex gap-4'>
+                                    {book.tags.map((tab, i) =>
+                                        <span
+                                            key={i}
+                                            className='py-2 px-4 bg-[#23BE0A10] text-[#23BE0A] rounded-full text-medium '>
+                                            {tab}
+                                        </span>)}
+                                </span>
+                                <p
+                                    className='text-[#13131380] flex items-center gap-2'>
+                                    <IoLocationOutline /> Year of Publishing: {book.yearOfPublishing}
+                                </p>
+                            </div>
+                            <div className='text-[#13131360] mb-4 flex items-center gap-6'>
+                                <span className='flex items-center gap-2'>
+                                    <MdManageAccounts />
+                                    <p>Publisher: {book.publisher}</p>
+                                </span>
+                                <span className='flex items-center gap-2'>
+                                    <GrDocumentUser />
+                                    <p>Pages: {book.totalPages}</p>
+                                </span>
+                            </div>
+                            <div className='border-t border-[#13131315] mb-4'></div>
+                            <div className='flex items-center gap-6'>
+                                <div className='px-5 py-3 rounded-full bg-[#328EFF15]'>
+                                    <p className='text-[#328EFF]'>Category: {book.category}</p>
+                                </div>
+                                <div className='px-5 py-3 rounded-full bg-[#FFAC3315]'>
+                                    <p className='text-[#FFAC33]'>Rating: {book.rating}</p>
+                                </div>
+
+                                <div className='px-5 py-3 rounded-full bg-[#23BE0A]'>
+                                    <p className='text-white font-medium'>View Details </p>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+            }
+        </div>
+    );
+};
+
+export default ReadBook;
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import ReadBook from '../../Components/ListingBook/ReadBook';
+import WishBook from '../../Components/ListingBook/WishBook';
 
 const Books = () => {
     return (
         <div>
-            this is books page
+            <div className='bg-[#13131310] rounded-2xl py-8 flex justify-center items-center mb-8'>
+                <p className='font-bold text-[#131313] text-2xl'>Books</p>
+            </div>
+            <div className='flex justify-center items-center mb-14'>
+                <button className='bg-[#23BE0A] rounded-lg btn text-white flex items-center '>Short By <MdKeyboardArrowDown className='text-2xl' />
+                </button>
+            </div>
+
+            <Tabs>
+                <TabList>
+                    <Tab>Read List</Tab>
+                    <Tab>Wishlist</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <ReadBook />
+                </TabPanel>
+                <TabPanel>
+                    <WishBook />
+                </TabPanel>
+            </Tabs>
         </div>
     );
 };
