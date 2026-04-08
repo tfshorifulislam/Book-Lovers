e8df5e5fad7c7a2be985b5a322d648b604675050
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-08 13:29:28 +0600

    read book list responsive issu fixed

--- src/Components/ListingBook/ReadBook.jsx
+++ src/Components/ListingBook/ReadBook.jsx
@@ -19,7 +19,8 @@ const ReadBook = () => {
                                 className='playfair font-bold text-2xl text-[#131313] mb-4'>{book.bookName}
                             </h1>
                             <p className='text-[#13131380] font-medium mb-4'>{book.author}</p>
-                            <div className='mb-4 flex items-center gap-4'>
+
+                            <div className='mb-4 flex flex-col md:flex-row md:items-center gap-4 text-start'>
                                 <p className='font-bold text-[#131313]'>Tag</p>
                                 <span className='flex gap-4'>
                                     {book.tags.map((tab, i) =>
@@ -34,7 +35,8 @@ const ReadBook = () => {
                                     <IoLocationOutline /> Year of Publishing: {book.yearOfPublishing}
                                 </p>
                             </div>
-                            <div className='text-[#13131360] mb-4 flex items-center gap-6'>
+
+                            <div className='text-[#13131360] mb-4 flex flex-col md:flex-row  md:items-center gap-6'>
                                 <span className='flex items-center gap-2'>
                                     <MdManageAccounts />
                                     <p>Publisher: {book.publisher}</p>
@@ -44,16 +46,18 @@ const ReadBook = () => {
                                     <p>Pages: {book.totalPages}</p>
                                 </span>
                             </div>
+
                             <div className='border-t border-[#13131315] mb-4'></div>
-                            <div className='flex items-center gap-6'>
-                                <div className='px-5 py-3 rounded-full bg-[#328EFF15]'>
-                                    <p className='text-[#328EFF]'>Category: {book.category}</p>
+
+                            <div className='flex flex-col md:flex-row md:items-center gap-6'>
+                                <div className='flex justify-center px-5 py-3 rounded-full bg-[#328EFF15]'>
+                                    <p className='flex justify-center text-[#328EFF]'>Category: {book.category}</p>
                                 </div>
-                                <div className='px-5 py-3 rounded-full bg-[#FFAC3315]'>
+                                <div className='flex justify-center  px-5 py-3 rounded-full bg-[#FFAC3315]'>
                                     <p className='text-[#FFAC33]'>Rating: {book.rating}</p>
                                 </div>
 
-                                <div className='px-5 py-3 rounded-full bg-[#23BE0A]'>
+                                <div className='flex justify-center  px-5 py-3 rounded-full bg-[#23BE0A]'>
                                     <p className='text-white font-medium'>View Details </p>
                                 </div>
                                 
